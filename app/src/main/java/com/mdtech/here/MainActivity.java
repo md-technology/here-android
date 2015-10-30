f8c11dca063d50165b63b1a33c46cd64e33bdbf7
anypossiblew <anypossible.w@gmail.com>
2015-10-30 16:15:02 +0800

    added map sytles and prepare to social interfaces

--- app/src/main/java/com/mdtech/here/MainActivity.java
+++ app/src/main/java/com/mdtech/here/MainActivity.java
@@ -1,5 +1,6 @@
 package com.mdtech.here;
 
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
@@ -13,7 +14,18 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.annotations.Sprite;
+import com.mapbox.mapboxsdk.annotations.SpriteFactory;
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.geometry.LatLngZoom;
+import com.mapbox.mapboxsdk.views.MapView;
+
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private MapView mapView = null;
+    private SpriteFactory spriteFactory = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +40,13 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                mapView.setCenterCoordinate(new LatLngZoom(new LatLng(mapView.getMyLocation()), 16));
+
+                mapView.addMarker(new MarkerOptions()
+                        .position(new LatLng(32.3, 121))
+                        .title("Test add marker")
+                        .icon(spriteFactory.fromResource(R.drawable.dic_launcher)));
+
             }
         });
 
@@ -39,6 +58,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //map
+        mapView = (MapView) findViewById(R.id.mapview);
+        mapView.setStyleUrl("asset://styles/amap-satellite-v8.json");
+//        mapView.setStyleUrl(Style.MAPBOX_STREETS);
+        mapView.setCenterCoordinate(new LatLng(40.73581, -1.99155));
+        mapView.setZoomLevel(5);
+        mapView.onCreate(savedInstanceState);
+
+        spriteFactory = new SpriteFactory(mapView);
     }
 
     @Override
@@ -63,11 +92,31 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        switch (item.getItemId()) {
+            case R.id.action_map_style_amap_satellite:
+                mapView.setStyleUrl("asset://styles/amap-satellite-v8.json");
+                return true;
+            case R.id.action_map_style_osm_water:
+                mapView.setStyleUrl("asset://styles/osm-water.json");
+                return true;
+            case R.id.action_map_style_dark:
+                mapView.setStyle(Style.DARK);
+                return true;
+            case R.id.action_map_style_emerald:
+                mapView.setStyle(Style.EMERALD);
+                return true;
+            case R.id.action_map_style_light:
+                mapView.setStyle(Style.LIGHT);
+                return true;
+            case R.id.action_map_style_mapbox_streets:
+                mapView.setStyle(Style.MAPBOX_STREETS);
+                return true;
+            case R.id.action_map_style_satellite:
+                mapView.setStyle(Style.SATELLITE);
+                return true;
+            case R.id.action_map_style_satellite_streets:
+                mapView.setStyle(Style.SATELLITE_STREETS);
+                return true;
         }
 
         return super.onOptionsItemSelected(item);
@@ -96,5 +145,41 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onPause()  {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
     }
 }
